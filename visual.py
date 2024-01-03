f9e50986c78add327c817feea39ec3e50e659226
Kulichkins <vova_levashov@inbox.ru>
2024-01-03 22:55:42 +0500

    visual_1.1

--- visual.py
+++ visual.py
@@ -1,14 +1,77 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 
-
+def add_window():
+    window = Tk()
+    window['bg'] = '#fafafa'
+    window.title('text-to-speech')
+    window.geometry('300x250')
+    window.resizable(width=FALSE,height=FALSE)
+    return(window)
 
 def button_click():
     login = loginInput.get()
     passwd = passField.get()
     if login != '' and passwd != '': 
-        info = f'login :{str(login)}, password:{str(passwd)}'
-        messagebox.showinfo(title='IFO',message=info)
+        
+        
+        
+        
+        
+        def button_click_audio():
+            print(your_bible.get())
+        
+        
+        
+        def button_click_text_to_speech():
+            print('its ok')
+        
+        
+        
+        
+        
+        window1.destroy()
+        new_window = add_window()
+        
+        new_canvas=Canvas(new_window, height=300, width=250)
+        new_canvas.pack
+
+
+        new_frame=Frame(new_window, bg='red')
+        new_frame.place(relx=0.15, rely=0.15, relwidth=0.7,relheight=0.7)
+
+        your_voice = [
+            'gggg',
+            'ddd',
+            'fff'
+        ]
+
+        selected_option = StringVar()
+        your_bible = ttk.Combobox(new_frame, textvariable=selected_option, values=your_voice, state='readonly')
+        your_bible.pack(pady=(20,0))
+
+
+        press_me_new = Button(new_frame, text = 'select', bg='grey',command=button_click_audio)
+        press_me_new.pack(pady=5)
+        
+
+
+        new_title = Label(new_frame, text='Enter your text',bg='grey',font=30)
+        new_title.pack(pady=(5,5))
+
+        textField = Entry(new_frame, bg='white')
+        textField.pack(pady=(0,5))
+
+        press_me_new1 = Button(new_frame, text = 'text to speech', bg='grey',command=button_click_text_to_speech)
+        press_me_new1.pack()
+
+
+
+
+
+        new_window.mainloop()
+        
     else:
         messagebox.showerror(title='sukablyat',message='you fucking stpd beatch')
 
@@ -17,19 +80,18 @@ def button_click():
 
 
 
-window = Tk()
-window['bg'] = '#fafafa'
-window.title('text-to-speech')
-window.geometry('300x250')
+window1 = add_window()
 
 
-window.resizable(width=FALSE,height=FALSE)
 
-canvas = Canvas(window, height=300, width=250)
+
+canvas = Canvas(window1, height=300, width=250)
 canvas.pack()
 
-frame = Frame(window,bg='red')
+frame = Frame(window1,bg='red')
 frame.place(relx=0.15, rely=0.15, relwidth=0.7,relheight=0.7)
+
+
 
 
 
@@ -48,9 +110,11 @@ passField.pack(pady=(0,2))
 
 
 
+
+
 press_me = Button(frame, text = 'confirm', bg='grey',command=button_click)
 press_me.pack()
 
 
 
-window.mainloop()
+window1.mainloop()
